98ed3cc44c504069c35b703c00742b1aaee57e07
Kiran S. Kedlaya <kskedl@gmail.com>
2026-05-05 20:44:41 -0700

    Most blueprint links now correct

--- VectorBundles/ArtinSchreier/ArtinSchreier.lean
+++ VectorBundles/ArtinSchreier/ArtinSchreier.lean
@@ -1,9 +1,11 @@
 module
 
+public import Mathlib.Algebra.Algebra.Basic
 public import Mathlib.Algebra.CharP.Defs
 public import Mathlib.Algebra.Group.Subgroup.ZPowers.Basic
 public import Mathlib.Algebra.Ring.Semireal.Defs
 public import Mathlib.Algebra.Ring.SumsOfSquares
+public import Mathlib.Algebra.CharP.Frobenius
 public import Mathlib.Data.Nat.Prime.Defs
 public import Mathlib.FieldTheory.AlgebraicClosure
 public import Mathlib.FieldTheory.Galois.Basic
@@ -13,6 +15,7 @@ public import Mathlib.FieldTheory.IsAlgClosed.Basic
 public import Mathlib.FieldTheory.IsAlgClosed.AlgebraicClosure
 public import Mathlib.FieldTheory.IsRealClosed.Basic
 public import Mathlib.FieldTheory.KummerExtension
+public import Mathlib.FieldTheory.Minpoly.Basic
 public import Mathlib.FieldTheory.Perfect
 public import Mathlib.FieldTheory.PurelyInseparable.Exponent
 public import Mathlib.FieldTheory.PurelyInseparable.PerfectClosure
@@ -20,6 +23,60 @@ public import Mathlib.FieldTheory.SplittingField.Construction
 public import Mathlib.GroupTheory.Perm.Cycle.Type
 
 @[expose] public section
+
+lemma quadratic_algebraic_closure_no_i (F : Type) (K : Type)
+  [Field F] [Field K] [Algebra F K] [FiniteDimensional F K] [IsAlgClosure F K]
+  (hi: ∃ (i : F), i^2 = -1) (h : Module.finrank F K = 2) :
+  ∀ (a : F), IsSquare a := by
+  have _: IsAlgClosed K := by
+    exact IsAlgClosure.isAlgClosed F
+  obtain ⟨i, hi⟩ := hi
+  intro a
+  let iota := algebraMap F K
+  have hb : ∃ b : K, b^4 = iota a := by
+    apply IsAlgClosed.exists_pow_nat_eq
+    simp
+  obtain ⟨b, hb⟩ := hb
+  have hint : IsIntegral F b := by
+    exact Algebra.IsIntegral.isIntegral b
+  let f := minpoly F b
+  let d := Polynomial.natDegree f
+  have _ : 0 < d := by
+    exact minpoly.natDegree_pos hint
+  have hf : d ≤ 2 := by
+    grind [minpoly.natDegree_le]
+  have hd : d = 1 ∨ d = 2 := by
+    grind
+  have haeval : Polynomial.aeval b f = 0 := by
+    exact minpoly.aeval F b
+  have hc : ∃ c : F, iota c = b^2 := by
+    cases hd
+    · have _ : (minpoly F b).natDegree = 1 := by
+        grind
+      have _ : (minpoly F b).natDegree = 1 ↔ b ∈ iota.range := by
+        exact minpoly.natDegree_eq_one_iff
+      have _ : b ∈ iota.range := by
+        (expose_names; exact minpoly.natDegree_eq_one_iff.mp h_3)
+      have hb₀ : ∃ b₀ : F, iota b₀ = b := by
+        (expose_names; exact Set.mem_range.mp h_6)
+      obtain ⟨b₀, hb₀⟩ := hb₀
+      use b₀^2
+      grind
+
+    · -- let f0 := Polynomial.constantCoeff f
+      let g := Polynomial.X^4 - Polynomial.C (a : F)
+      have _ : Polynomial.aeval b g = 0 := by
+        sorry
+      have _ : f ∣ g := by
+        sorry
+      sorry
+  unfold IsSquare
+  obtain ⟨c, hc⟩ := hc
+  use c
+  have hinj : Function.Injective iota := by
+    exact FaithfulSMul.algebraMap_injective F K
+  apply hinj
+  grind
 
 lemma finite_algebraic_closure_separable (F : Type) (K : Type)
   [Field F] [Field K] [Algebra F K] [FiniteDimensional F K]
@@ -35,33 +92,40 @@ lemma finite_algebraic_closure_separable (F : Type) (K : Type)
       have _: ExpChar F p := by
         (expose_names; exact ExpChar.prime h_2)
       let frob := frobenius F p
-      have _: Function.Surjective frob := by
-        unfold Function.Surjective
-        unfold frob
+      have hpow: ∀ a : F, ∃ b : F, b^p = a := by
         intro a
-        let d := Module.finrank F K
-        have _ : 1 < p := by
-          (expose_names; exact Nat.Prime.one_lt h_2)
-        have h1: ∃ n : ℕ, d < p^n := by
-          refine pow_unbounded_of_one_lt d ?_
-          grind
-        obtain ⟨n, h2⟩ := h1
-        have _: IsAlgClosed K := by
-          exact IsAlgClosure.isAlgClosed F
-        have h3: ∃ b : K, b^(p^n) = (algebraMap F K) a := by
-          refine IsAlgClosed.exists_pow_nat_eq ((algebraMap F K) a) ?_
-          grind
-        obtain ⟨b, h4⟩ := h3
-        let f := minpoly F b
-        have _ : ¬ Irreducible f := by
+        by_cases hp: p = 2
+        · sorry
+          --   sorry
+            -- apply quadratic_algebraic_closure_no_i F K a
+        · let d := Module.finrank F K
+          have _ : 1 < p := by
+            (expose_names; exact Nat.Prime.one_lt h_2)
+          have h1: ∃ n : ℕ, d < p^n := by
+            refine pow_unbounded_of_one_lt d ?_
+            grind
+          obtain ⟨n, h2⟩ := h1
+          have _: IsAlgClosed K := by
+            exact IsAlgClosure.isAlgClosed F
+          have h3: ∃ b : K, b^(p^n) = (algebraMap F K) a := by
+            refine IsAlgClosed.exists_pow_nat_eq ((algebraMap F K) a) ?_
+            grind
+          obtain ⟨b, h4⟩ := h3
+          let f := minpoly F b
+          have _ : ¬ Irreducible f := by
+            sorry
+          let g := Polynomial.factor f
+          let d₁ := Polynomial.natDegree g
+          have _ : d₁ < p^n := by
+            sorry
+          let e := gcd d₁ p^n
+          let (g_1, g_2) := Nat.xgcd d₁ p^n
           sorry
-        let g := Polynomial.factor f
-        let d₁ := Polynomial.natDegree g
-        have _ : d₁ < p^n := by
-          sorry
-        let e := gcd d₁ p^n
-        let (g_1, g_2) := Nat.xgcd d₁ p^n
-        sorry
+      have _: Function.Surjective frob := by
+          unfold Function.Surjective
+          unfold frob
+          intro a
+          apply hpow
       have _: PerfectRing F p := by
         (expose_names; exact PerfectRing.ofSurjective F p h_4)
       exact PerfectRing.toPerfectField F p
@@ -77,11 +141,6 @@ lemma finite_algebraic_closure_cyclic_quadratic (F : Type) (K : Type) (p : ℕ)
   [Field F] [Field K] [Algebra F K] [FiniteDimensional F K] [IsAlgClosure F K]
   (hp: Nat.Prime p) (hrank: Module.finrank F K = p)
   (hgal: IsGalois F K): p = 2 := by
-  sorry
-
-lemma quadratic_algebraic_closure_no_i (F : Type) (K : Type)
-  [Field F] [Field K] [Algebra F K] [FiniteDimensional F K] [IsAlgClosure F K]
-  (h : Module.finrank F K = 2) : ¬∃ (i : F), i^2 = -1 := by
   sorry
 
 lemma finite_algebraic_closure_with_i (F : Type) (K : Type)
@@ -114,16 +173,14 @@ lemma finite_algebraic_closure_with_i (F : Type) (K : Type)
   have hgal1: IsGalois F₁ K :=
     finite_algebraic_closure_separable F₁ K
   have hp2: p = 2 :=
-      finite_algebraic_closure_cyclic_quadratic F₁ K p hp hrank hgal1
-  apply quadratic_algebraic_closure_no_i F₁ K
-  grind
-  rcases h with ⟨i, hi⟩
-  let iota := algebraMap F F₁
-  have h2 : Function.Injective iota := by
-    exact FaithfulSMul.algebraMap_injective F ↥F₁
-  let i₁ := iota i
-  use i₁
+    finite_algebraic_closure_cyclic_quadratic F₁ K p hp hrank hgal1
+  have ha: ∃ a : F₁, ¬ IsSquare a := by
+    sorry
+  obtain ⟨a, ha⟩ := ha
+  have hfin: Module.finrank F₁ K = 2 := by
+    sorry
   sorry
+  -- apply quadratic_algebraic_closure_no_i F₁ K
 
 lemma RealClosed_from_quadratic (F : Type) (K : Type)
   [Field F] [Field K] [Algebra F K] [FiniteDimensional F K] [IsAlgClosed K]
