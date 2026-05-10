15efe8ca66988beeb25ad551acaf26022b9c2e66
Kiran S. Kedlaya <kskedl@gmail.com>
2026-05-10 16:54:13 -0700

    Lemma on nonsquares in quadratic extensions

--- VectorBundles/ArtinSchreier/ArtinSchreier3.lean
+++ VectorBundles/ArtinSchreier/ArtinSchreier3.lean
@@ -33,15 +33,11 @@ public import Mathlib.GroupTheory.Perm.Cycle.Type
 
 public import VectorBundles.ArtinSchreier.ArtinSchreier
 public import VectorBundles.ArtinSchreier.ArtinSchreier2
+public import VectorBundles.ArtinSchreier.FieldTheory
 
 @[expose] public section
 
-lemma cyclic_char_p_as_artin_schreier (F : Type) (K : Type) (p : ℕ)
-  [Field F] [Field K] [Algebra F K] [FiniteDimensional F K]
-  (hp: Nat.Prime p) (hrank: Module.finrank F K = p)
-  (hsep: IsGalois F K) (hchar: ringChar F = p) : ∃ (a : F), ∃ (x : K),
-  minpoly F x = Polynomial.X ^ p - Polynomial.X - Polynomial.C a := by
-  sorry
+open IntermediateField
 
 lemma finite_algebraic_closure_cyclic_prime (F : Type) (K : Type) (p : ℕ)
   [Field F] [Field K] [Algebra F K] [FiniteDimensional F K] [IsAlgClosure F K]
@@ -59,7 +55,61 @@ lemma finite_separable_algebraic_closure_with_i (F : Type) (K : Type)
   [Field F] [Field K] [Algebra F K] [FiniteDimensional F K]
   [Algebra.IsSeparable F K] [IsAlgClosure F K]
   (h : ∃ (i : F), i^2 = -1) : IsAlgClosed F := by
-  sorry
+  have _ : IsGalois F K := by
+    (expose_names; exact { to_isSeparable := inst_4, to_normal := IsAlgClosure.normal F K })
+  let G := Gal(K/F)
+  have hG : Nat.card G = 1 := by
+    by_contra
+    let d := Nat.card G
+    have hp : ∃ p : ℕ, Nat.Prime p ∧ p ∣ d := by
+      exact Nat.exists_prime_and_dvd this
+    obtain ⟨p, hp1, hp2⟩ := hp
+    have hp2 : Fact (Nat.Prime p) := by
+      exact fact_iff.mpr hp1
+    have hp3 : Fact (p.Prime) := by
+      exact fact_iff.mpr hp1
+    have hg : ∃ g : G, orderOf g = p := by
+      apply exists_prime_orderOf_dvd_card
+      have _ : Nat.card G = Fintype.card G := by
+        exact Nat.card_eq_fintype_card
+      subst d
+      simp_all
+    obtain ⟨g, hg⟩ := hg
+    let H := Subgroup.zpowers g
+    let E := IntermediateField.fixedField H
+    have _ : Nat.card H = p := by
+      sorry
+    have h_ekrank : Module.finrank E K = p := by
+      sorry
+    have _ : IsAlgClosure E K := by
+      refine { isAlgClosed := ?_, isAlgebraic := ?_ }
+      exact IsAlgClosure.isAlgClosed F
+      exact IntermediateField.isAlgebraic_tower_top
+    have hgal: IsGalois E K := by
+      exact IsGalois.tower_top_intermediateField E
+    have _ : p = 2 := by
+      apply finite_algebraic_closure_cyclic_quadratic E K
+      exact hp1
+      exact Eq.symm ((fun {a b} => Nat.succ_inj.mp) (congrArg Nat.succ (id (Eq.symm h_ekrank))))
+      apply hgal
+    have h_char2 : ringChar E ≠ 2 := by
+      apply finite_algebraic_closure_cyclic_prime E K
+      simp_all
+      simp_all
+      exact hgal
+    have h_sq : ∀ (a : E), IsSquare a := by
+      have h_sq1 : ∀ (a : E), IsSquare a ∨ IsSquare (-a) := by
+        apply quadratic_algebraic_closure_no_i E K
+        simp_all
+      sorry
+    have _ : FiniteDimensional E K := by
+      exact finiteDimensional_right E
+    have h_sq3 : ∃ (a: E), ¬ IsSquare a := by
+      apply nonsquare_in_quadratic_extension E K
+      simp_all
+      exact h_char2
+    simp_all
+  exact trivial_absolute_galois_group F K hG
 
 lemma finite_inseparable_algebraic_closure_with_i (F : Type) (K : Type)
   [Field F] [Field K] [Algebra F K] [FiniteDimensional F K]
---
+++ VectorBundles/ArtinSchreier/FieldTheory.lean
@@ -0,0 +1,166 @@
+module
+
+public import Mathlib.Algebra.Algebra.Basic
+public import Mathlib.Algebra.CharP.Defs
+public import Mathlib.Algebra.Group.Subgroup.ZPowers.Basic
+public import Mathlib.Algebra.Polynomial.Splits
+public import Mathlib.Algebra.Ring.Semireal.Defs
+public import Mathlib.Algebra.Ring.SumsOfSquares
+public import Mathlib.Algebra.CharP.Frobenius
+public import Mathlib.Data.Nat.Prime.Defs
+public import Mathlib.FieldTheory.AlgebraicClosure
+public import Mathlib.FieldTheory.Galois.Basic
+public import Mathlib.FieldTheory.Galois.IsGaloisGroup
+public import Mathlib.FieldTheory.IntermediateField.Adjoin.Defs
+public import Mathlib.FieldTheory.IntermediateField.Basic
+public import Mathlib.FieldTheory.IsAlgClosed.Basic
+public import Mathlib.FieldTheory.IsAlgClosed.AlgebraicClosure
+public import Mathlib.FieldTheory.IsRealClosed.Basic
+public import Mathlib.FieldTheory.KummerExtension
+public import Mathlib.FieldTheory.KummerPolynomial
+public import Mathlib.FieldTheory.Minpoly.Basic
+public import Mathlib.FieldTheory.Minpoly.MinpolyDiv
+public import Mathlib.FieldTheory.Perfect
+public import Mathlib.FieldTheory.PrimitiveElement
+public import Mathlib.FieldTheory.PurelyInseparable.Basic
+public import Mathlib.FieldTheory.PurelyInseparable.Exponent
+public import Mathlib.FieldTheory.PurelyInseparable.PerfectClosure
+public import Mathlib.FieldTheory.PurelyInseparable.Tower
+public import Mathlib.FieldTheory.SeparableClosure
+public import Mathlib.FieldTheory.SplittingField.Construction
+public import Mathlib.FieldTheory.Relrank
+public import Mathlib.GroupTheory.Perm.Cycle.Type
+
+@[expose] public section
+
+open IntermediateField
+
+lemma cyclic_char_p_as_artin_schreier (F : Type) (K : Type) (p : ℕ)
+  [Field F] [Field K] [Algebra F K] [FiniteDimensional F K]
+  (hp: Nat.Prime p) (hrank: Module.finrank F K = p)
+  (hsep: IsGalois F K) (hchar: ringChar F = p) : ∃ (a : F), ∃ (x : K),
+  minpoly F x = Polynomial.X ^ p - Polynomial.X - Polynomial.C a := by
+  sorry
+
+lemma nonsquare_in_quadratic_extension (F : Type) (K : Type)
+  [Field F] [Field K] [Algebra F K] [FiniteDimensional F K]
+  (hrank: Module.finrank F K = 2) (hchar: ringChar F ≠ 2) :
+  ∃ a : F, ¬ IsSquare a := by
+
+  have _ : ringChar F = ringChar K := by
+    exact Algebra.ringChar_eq F K
+  have _ : CharP F (ringChar K) := by
+    (expose_names; exact ringChar.of_eq h)
+  have prim : (primitiveRoots (Module.finrank F K) F).Nonempty := by
+    unfold primitiveRoots
+    refine Finset.nonempty_def.mpr ?_
+    use -1
+    simp_all
+    refine IsPrimitiveRoot.neg_one (ringChar K) ?_
+    simp_all
+  have _ : Algebra.IsQuadraticExtension F K := by
+    exact { toFree := Module.free_of_finite_type_torsion_free', finrank_eq_two' := hrank }
+  have _ : Algebra.IsSeparable F K := by
+    let d := Field.finInsepDegree F K
+    let e := Field.finSepDegree F K
+    let q := ringExpChar F
+    have hd : d ∣ 2 := by
+      have h_de : Field.finSepDegree F K * Field.finInsepDegree F K = Module.finrank F K := by
+        exact Field.finSepDegree_mul_finInsepDegree F K
+      have _ : e * d = Module.finrank F K := by
+        subst d e
+        apply h_de
+      have _ : e * d = 2 := by
+        simp_all
+      (expose_names; exact dvd_of_mul_left_eq e h_4)
+    have hf : ∃ f : ℕ, d = q^f := by
+      apply finInsepDegree_eq_pow
+    obtain ⟨f, hf⟩ := hf
+    by_cases d = 1
+    · (expose_names; exact (isSeparable_iff_finInsepDegree_eq_one F K).mpr h_3)
+    · have h_f : d = 2 := by
+        have _ : Nat.Prime 2 := by
+          exact Nat.prime_two
+        have _ : Nat.Prime 2 → (d ∣ 2 ↔ d = 1 ∨ d = 2) :=
+          Nat.dvd_prime
+        simp_all
+      have h_chars : ∀ (p q : ℕ) [hp : CharP F p] [hq : ExpChar F q], p = q ↔ Nat.Prime p :=
+        char_eq_expChar_iff F
+      have _ : CharP F (ringChar F) := by
+        exact ringChar.charP F
+      specialize h_chars (ringChar F) q
+      have h_pq : Nat.Prime (ringChar F) ∧ Nat.Prime q ∧ (ringChar F) = q := by
+        have h_chars2 : ∀ (p q : ℕ) [CharP F p] [ExpChar F q], q = 1 ↔ p = 0 :=
+          expChar_one_iff_char_zero F
+        specialize h_chars2 (ringChar F) q
+        have h_chars3 : ∀ (p : ℕ) [hc : CharP F p], Nat.Prime p ∨ p = 0 :=
+          CharP.char_is_prime_or_zero F
+        specialize h_chars3 (ringChar F)
+        simp_all
+      obtain ⟨h_pq1, h_pq2, h_pq3⟩ := h_pq
+      exfalso
+      have _ : f ≥ 1 := by
+        by_contra
+        push Not at this
+        have _ : f = 0 := by
+          simp_all
+        have _ : q ^ f = 1 := by
+          (expose_names;
+            exact pow_eq_one_iff_modEq.mpr (congrFun (congrArg HMod.hMod h_5) (orderOf q)))
+        simp_all
+      have h_q2 : q ∣ 2 := by
+        have _ : f ≥ 1 → q^1 ∣ q^f := by
+          (expose_names; exact fun a => Nat.pow_dvd_pow q h_5)
+        grind
+      have _ : q = 2 := by
+        have _ : Nat.Prime 2 := by
+          exact Nat.prime_two
+        have _ : Nat.Prime 2 → (q ∣ 2 ↔ q = 1 ∨ q = 2) :=
+          Nat.dvd_prime
+        have _ : q ≠ 1 := by
+          by_contra
+          have _ : q ^ f = 1 := by
+            refine Nat.pow_eq_one.mpr ?_
+            left
+            exact this
+          grind
+        grind
+      grind
+  have _ : IsGalois F K := by
+    (expose_names;
+      exact { to_isSeparable := h_3, to_normal := Algebra.IsQuadraticExtension.normal F K })
+  have _ : IsCyclic Gal(K/F) := by
+    (expose_names; refine Algebra.IsQuadraticExtension.isCyclic F K)
+  have hb : ∃ (b : K), b ^ Module.finrank F K ∈ Set.range (algebraMap F K) ∧ F⟮b⟯ = (⊤: IntermediateField F K) :=
+    exists_root_adjoin_eq_top_of_isCyclic F K prim
+  obtain ⟨b, hb1, hb2⟩ := hb
+  have ha : ∃ (a : F), b ^ 2 = (algebraMap F K) a := by
+    simp_all
+    obtain ⟨a, hb1⟩ := hb1
+    use a
+    symm
+    exact hb1
+  obtain ⟨a, ha⟩ := ha
+  use a
+  by_contra
+  unfold IsSquare at this
+  obtain ⟨c, this⟩ := this
+  have hb3 : (algebraMap F K) c = b ∨ (algebraMap F K) (-c) = b := by
+    grind
+  have _ : b ∈ (algebraMap F K).range := by
+    refine RingHom.mem_range.mpr ?_
+    cases hb3
+    · use c
+    · use -c
+  have _ : F⟮b⟯ = (⊥: IntermediateField F K) := by
+    (expose_names; exact adjoin_simple_eq_bot_iff.mpr h_6)
+  have _ : Module.finrank F K = 1 := by
+    have _ : (⊥: IntermediateField F K) = (⊤: IntermediateField F K) := by
+      simp_all
+    (expose_names; exact bot_eq_top_iff_finrank_eq_one.mp h_8)
+  simp_all
+
+lemma trivial_absolute_galois_group (F : Type) (K : Type)
+  [Field F] [Field K] [Algebra F K] [Algebra.IsSeparable F K] [IsAlgClosure F K]
+  (h : Nat.card Gal(K/F) = 1) : IsAlgClosed F := by
+  sorry
